4715ed592bf6e9691c26d3cbaa0c0a0b1a6d7ead
David J. Burger <davidburger@gmail.com>
2024-05-16 08:32:27 -0700

    Separate site reloads into individual timeouts
    Separate the site reloads into individual timeouts so that
    in a future improvement the sites can be configured to
    reload on individual timing schedules.

--- src/background.js
+++ src/background.js
@@ -1,26 +1,75 @@
 importScripts("./common.js");
 
-const reload = () => {
-    console.log("reloading start...");
-    getSettings(settings => {
-        const domains = new Set(settings.domains);
-        chrome.tabs.query({url: "https://*/*"}, (tabs) => {
-            for (const tab of tabs) {
-                const host = new URL(tab.url).host;
-                if (domains.has(host)) {
-                    if (tab.active) {
-                        console.log("noreload", tab.url, "tab was active");
-                    } else {
-                        chrome.tabs.reload(tab.id, {bypassCache: false}, () => {
-                            console.log("reloaded", tab.url);
-                        });
-                    }
+// Map of domains to timeout ID for the reload timeout.
+const sites = new Map();
+
+/**
+ * Returns the milliseconds value for the given seconds.
+ *
+ * @param seconds {number} - Seconds to convert to milliseconds.
+ * @returns {number} - The number of milliseconds.
+ */
+const seconds2Millis = (seconds) => {
+    return seconds * 1000;
+};
+
+/**
+ * Reloads tabs from the given domain, if any, and sets the next
+ * reload timeout for that domain.
+ *
+ * @param domain {string} - The domain to reload tabs for.
+ */
+const reload = (domain) => {
+    console.log("looking for", domain);
+    let found = false;
+    chrome.tabs.query({url: "https://*/*"}, (tabs) => {
+        for (const tab of tabs) {
+            const host = new URL(tab.url).host;
+            if (host === domain) {
+                found = true;
+                if (tab.active) {
+                    console.log(domain, "not reloaded, tab active", tab.url);
                 } else {
-                    console.log("noreload", tab.url, "no host match");
+                    chrome.tabs.reload(tab.id, {bypassCache: false}, () => {
+                        console.log(domain, "reloaded", tab.url);
+                    });
                 }
             }
-        });
+        }
+        if (!found) {
+            console.log(domain, "not found");
+        }
+    });
+    sites.set(domain, setTimeout(reload, seconds2Millis(300), domain));
+};
+
+/**
+ * Syncs the sites map with sites from the current settings.
+ */
+const syncSites = () => {
+    console.log("syncing sites");
+
+    getSettings(settings => {
+        const domains = new Set(settings.domains);
+
+        // Add sites not present.
+        for (const domain of domains) {
+            if (!sites.has(domain)) {
+                const timeoutId = setTimeout(reload, seconds2Millis(300), domain);
+                sites.set(domain, timeoutId);
+                console.log("reload timeout added for", domain);
+            }
+        }
+
+        // Remove sites no longer in the settings.
+        for (const [domain, timeoutId] of sites) {
+            if (!domains.has(domain)) {
+                clearTimeout(timeoutId);
+                sites.delete(domain);
+                console.log("reload timeout removed for", domain);
+            }
+        }
     });
 };
 
-setInterval(reload, 300 * 1000);
+setInterval(syncSites, seconds2Millis(10));
